342b4e3d02d31f1b28c569247c62d8e303956bb9
Joelis Jimenez <joejimez@gmail.com>
2024-10-19 11:21:07 -0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+precio = 400000
+
+precioInicial = document.querySelector(".precio-inicial")
+precioInicial.innerHTML = precio.toLocaleString()
+
+//contador cantidad
+parrafo = document.querySelector('.cantidad')
+cantidad = Number(parrafo.innerHTML)
+
+//total a pagar
+valortotal = document.querySelector(".valor-total")
